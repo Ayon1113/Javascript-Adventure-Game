1741fe961e944a647c004afc363fb62a80453bf6
Ayon1113 <94404996+Ayon1113@users.noreply.github.com>
2021-12-10 15:34:46 -0500

    Added button text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("script is loading");
+let myText = {
+id: 1,
+  story: "Lee while scavenging for supplies has wound up in a building surrounded by zombies",
+  options: ["Find a weapon", "Make a run for it", "Barricade the entrances", "Crouch and stay sneaky"],
+};
