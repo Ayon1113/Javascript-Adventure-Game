e5f355f4c5dc0f3bcf05d11c4764bb2ad23ab32b
Ayon1113 <94404996+Ayon1113@users.noreply.github.com>
2021-12-14 14:01:48 -0500

    Used micposso's javascript text adventure game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 console.log("script is loading");
-let myText = {
-id: 1,
-  story: "Lee while scavenging for supplies has wound up in a building surrounded by zombies",
-  options: ["Find a weapon", "Make a run for it", "Barricade the entrances", "Crouch and stay sneaky"],
-};
+
+let adventureDescriptionText = document.querySelector(".adventure-description");
+let optionButtons = document.getElementsByClassName("button");
+
+function updateAdventureText() {
+  adventureDescriptionText.innerText = textAdventure[0].text;
+}
+
+function updateOptions() {
+  let i = 0;
+  for(i; i < optionButtons.length; i++) {
+    console.log(optionButtons[i]);
+    optionButtons[i].innerText = textAdventure[0].options[i];
+  }
+}
+
+const textAdventure = [
+  {
+    id: 1,
+    text: 'Lee while scavenging for supplies has wound up in a building surrounded by zombies',
+    options: ["Make a break for the exit" , "Stay silent and sneak around" ]
+  },
+]
+
+updateAdventureText();
+updateOptions();
