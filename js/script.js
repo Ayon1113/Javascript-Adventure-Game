256da280f069854f497cb6408e3fb7b732145cbd
Ayon1113 <94404996+Ayon1113@users.noreply.github.com>
2021-12-15 15:36:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,51 @@ console.log("script is loading");
 
 let adventureDescriptionText = document.querySelector(".adventure-description");
 let optionButtons = document.getElementsByClassName("button");
+
+let startIndex = 1;
+
 function startStory(){
   console.log("Lee's story has begun");
-  showAdventureOptions(1);
+  let ids = textAdventure.map((item) => {
+return item.id;
+  });
+
+  if(startIndex > ids.length-1){
+    startIndex = 0;
+  }
+  showAdventureOptions(ids[startIndex]);
 }
 
 function showAdventureOptions(textAdventureID) {
   const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
+  if(!newDescriptionText.options){
+
+    document.getElementById("first_btn").style.display = "none";
+    adventureDescriptionText.innerHTML = newDescriptionText.text;
+
+    document.getElementById("second_btn").innerHTML = "Play again";
+    return;
+  }
   adventureDescriptionText.innerText = newDescriptionText.description;
+
   for(let i = 0; i < newDescriptionText.options.length; i++){
     optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
 
   }
 
 }
+function onNextOption(event){
+  event.preventDefault();
+  startIndex++;
+  startStory();
+}
+function onPlayAgain(event){
+  event.preventDefault();
+  document.getElementById("first_btn").style.display = "inline";
+  startIndex = 0;
+  startStory();
 
-
+}
 
 const textAdventure = [
   {
@@ -25,12 +54,12 @@ const textAdventure = [
     text: 'Lee while scavenging for supplies has wound up in a building surrounded by zombies, what should he do?',
     options: [
       {
-        buttontext = 'Make a break for the exit',
+        buttontext = 'Crouch and stay silent',
         setState: {break: true},
         nexttext: 2
       },
       {
-        buttontext: 'Crouch and stay silent',
+        buttontext: 'Make a break for the exit',
         nextText: 2
       }
      ]
@@ -40,12 +69,12 @@ const textAdventure = [
     text: 'Now that he has chosen to sneak around he must figure out which way to go',
     options: [
       {
-        buttontext = 'Head towards the front of the store',
+        buttontext = 'Head towards the back of the store',
         setState: {break: true},
         nexttext: 3
       },
       {
-        buttontext: 'Head towards the back of the store',
+        buttontext: 'Head towards the front of the store',
         nextText: 3
       }
      ]
@@ -55,17 +84,23 @@ const textAdventure = [
     text: 'Lee has made it to a room in the back of the store and encountered a zombie, should he shoot it or hit it with a bat?',
     options: [
       {
-        buttontext = 'Shoot it',
+        buttontext = 'Hit it with a bat',
         setState: {break: true},
         
       },
       {
-        buttontext: 'Hit it with a bat',
+        buttontext: 'Shoot it',
         
       }
      ]
   },
+  {
+    id: 4,
+    text: 'Lee lives to see another day'
+
+  },
 
 ];
 startStory();
+
 
