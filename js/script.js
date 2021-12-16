080e503de6d0cccb614a7b41babe0f0a1f0f4116
Ayon1113 <94404996+Ayon1113@users.noreply.github.com>
2021-12-16 14:07:01 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,107 +4,150 @@ console.log("script is loading");
 const descriptionText = document.querySelector(".adventure-description");
 const optionButtons = document.getElementsByClassName("button");
 
-let startIndex = 1;
+// Id node where the game starts
+let startIndex = 0;
 
-function startStory(){
-  console.log("Lee's story has begun");
-  let ids = textAdventure.map((item) => {
-return item.id;
-  });
-
-  if(startIndex > ids.length-1){
-    startIndex = 0;
-  }
-  showAdventureOptions(ids[startIndex]);
+// function that starts the game
+function startAdventure(textAdventureIndex) {
+  console.log("Adventure has started");
+  showAdventureOptions(textAdventureIndex);
 }
-
 
 // create a function that shows the description
 function showAdventureOptions(textAdventureID) {
-  const newDescriptionText = textAdventure.find(gameNode => gameNode.id === textAdventureID);
-  if(!newDescriptionText.options){
+  // check fisrt
 
+  const newDescriptionText = textAdventure.find(
+    (gameNode) => gameNode.id === textAdventureID
+  );
+  if (!newDescriptionText.options) {
+    // hide button
     document.getElementById("first_btn").style.display = "none";
-    adventureDescriptionText.innerHTML = newDescriptionText.text;
 
-    document.getElementById("second_btn").innerHTML = "Play again";
+    // show description
+    descriptionText.innerHTML = newDescriptionText.message;
+
+    document.getElementById("second_btn").innerHTML = "Play Again";
     return;
   }
-  adventureDescriptionText.innerText = newDescriptionText.description;
 
-  for(let i = 0; i < newDescriptionText.options.length; i++){
+  descriptionText.innerText = newDescriptionText.description;
+  // iterate through the buttons and insert the option text in each button
+  for (let i = 0; i < newDescriptionText.options.length; i++) {
+    //console.log(newDescriptionText.options[i].buttontext);
     optionButtons[i].innerText = newDescriptionText.options[i].buttontext;
+  }
+}
 
+// function that tracks options and passes the id to the showAdventureOptions function
+function buttonOptions() {
+  const adventureIndex = textAdventure.find(
+    (gameNode) => gameNode.id === startIndex
+  );
+
+  for (let i = 0; i < optionButtons.length; i++) {
+    optionButtons.addEventListener("click", function () {});
+  }
+}
+
+function onPlay() {
+  let ids = textAdventure.map((item) => {
+    return item.id;
+  });
+
+  if (startIndex > ids.length - 1) {
+    startIndex = 0;
   }
 
+  // show
+  showAdventureOptions(ids[startIndex]);
 }
-function onNextOption(event){
+
+function onNextOption(event) {
   event.preventDefault();
+
   startIndex++;
-  startStory();
+
+  onPlay();
 }
-function onPlayAgain(event){
+
+function onPlayAgain(event) {
   event.preventDefault();
-  document.getElementById("first_btn").style.display = "inline";
-  startIndex = 0;
-  startStory();
 
+  // show button
+  document.getElementById("first_btn").style.display = "inline";
+
+  startIndex = 0;
+
+  onPlay();
 }
 
+// optional: function that shows end of the game
 
 // create and object that holds the text for our game and options and also tracks state
 const textAdventure = [
   {
     id: 1,
-    description: 'This is the story of Lee who has been surrounded by zombies while trying to gather supplies in a store, what should he do?',
+    description:
+      "This is the story of Lee who while looking for supplies in a store has been surrounded by zombies, what should he do?",
     options: [
       {
-        text: "Crouch and stay silent",
+        buttontext: "Crouch and stay quiet",
         setState: { headback: true },
-        nexttext: 2
+        nexttext: 2,
       },
       {
-        text: "Make a break for the exit",
-        nextText: 2       
-      }
-    ]
+        buttontext: "Make a break for the exit",
+        message: console.log("Try again"),
+      },
+    ],
   },
   {
     id: 2,
-    description: 'Lee is now sneaking around the store but doesnt know which way to go',
+    description:
+      "Lee is now sneaking around the store but he needs to figure out where to go",
     options: [
       {
-        text: "Head towards the back of the store",
+        buttontext: "Head towards the back of the store",
         setState: { headback: true },
-        nexttext: 3
+        nexttext: 3,
       },
       {
-        text: "Head towards the front of the store",
-        nextText: 3       
-      }
-    ]
+        buttontext: "Head towards the front of the store",
+        message: console.log("Try again"),
+      },
+    ],
   },
   {
     id: 3,
-    description: 'Lee has made it to the back of the store but the door is being blocked by a zombie, what should he do?',
+    description:
+      "Lee has made it to the back of the store, and there is a door being blocked by a zombie, what should he do?",
     options: [
       {
-        text: "Hit it with a hammer",
+        buttontext: "Distract it with noise",
         setState: { headback: true },
-        nexttext: 4
       },
       {
-        text: "Shoot it",
-        nextText: 4       
-      }
-    ]
+        buttontext: "Shoot it",
+        setState: { daylight: true },
+        nexttext: 4,
+      },
+    ],
   },
   {
     id: 4,
-    text: "Lee has survived another day",
-    
-  }
-  
+    // message: console.log('Thank you for helping Juan!')
+    message: "Lee has lived to see another day",
+  },
 ];
 
-startStory();
+// get first index id
+// let ids = textAdventure.map(item => {
+//   return item.id;
+// });
+
+// let firstId = ids[startIndex];
+
+// startAdventure(firstId);
+
+onPlay();
